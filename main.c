8c580c599d86ce578a29418429ef0f6306872b5c
Jung Soo Choi <chlwjdtn15@hotmail.com>
2020-07-02 02:44:07 +0000

    구조체 주소 확인

--- main.c
+++ main.c
@@ -5,24 +5,33 @@
 struct Person {
   int age;
   char* name;
+
+
 };
 
 
 int main(void) {
 
   int arr[3];
-  
+
   struct Person p1;
+
+  printf("%ld\n", (long)p1.age);
+  printf("%ld\n", (long)p1.name);
 
   p1.name = "홍길동";
   p1.age = 22;
 
+  printf("%ld\n", (long)p1.age);
+  printf("%ld\n", (long)p1.name);
+
   struct Person p2 = p1;
+  printf("%ld\n", (long)p2.age);
+  printf("%ld\n", (long)p2.name);
 
-  
 
 
-  
+
 
   return 0;
 }
