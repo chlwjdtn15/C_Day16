1578ed5d569fa26eda48f2f9b86f9f59e056c705
Jung Soo Choi <chlwjdtn15@hotmail.com>
2020-07-02 02:51:21 +0000

    구조체 변수 수정하기

--- main.c
+++ main.c
@@ -2,32 +2,71 @@
 
 #include <stdio.h>
 
-struct Person {
-  int age;
+struct School {
+
+
   char* name;
-
-
+  int birth_date;
 };
+
+void change(struct School* str) {
+
+
+
+  str -> name = "미국초등학교";
+  str -> birth_date = 20180717;
+
+
+}
+
+
+// struct Person {
+//   int age;
+//   char* name;
+
+
+// };
 
 
 int main(void) {
 
-  int arr[3];
+  // int arr[3];
 
-  struct Person p1;
+  // struct Person p1;
 
-  printf("%ld\n", (long)p1.age);
-  printf("%ld\n", (long)p1.name);
+  // printf("%ld\n", (long)p1.age);
+  // printf("%ld\n", (long)p1.name);
 
-  p1.name = "홍길동";
-  p1.age = 22;
+  // p1.name = "홍길동";
+  // p1.age = 22;
 
-  printf("%ld\n", (long)p1.age);
-  printf("%ld\n", (long)p1.name);
+  // printf("%ld\n", (long)p1.age);
+  // printf("%ld\n", (long)p1.name);
 
-  struct Person p2 = p1;
-  printf("%ld\n", (long)p2.age);
-  printf("%ld\n", (long)p2.name);
+  // struct Person p2 = p1;
+  // printf("%ld\n", (long)p2.age);
+  // printf("%ld\n", (long)p2.name);
+
+
+// 문제 : 구조체 변수의 주소를 이용해서 원본값을 변경하는 함수를 구현해주세요.
+// 조건 : 아래와 같이 출력하게 해주세요.
+
+
+  struct School s1;
+  s1.name = "한국초등학교";
+  s1.birth_date = 19860404;
+
+  printf("학교의 이름 : %s\n", s1.name);
+  // 출력 => 학교의 이름 : 한국초등학교
+  printf("학교의 설립일 : %d\n", s1.birth_date);
+  // 출력 => 학교의 설립일 : 19860404
+
+  change(&s1);
+
+  printf("학교의 이름 : %s\n", s1.name);
+  // 출력 => 학교의 이름 : 미국초등학교
+  printf("학교의 설립일 : %d\n", s1.birth_date);
+  // 출력 => 학교의 설립일 : 20180717
 
 
 
